202a368b4a6510ae6b87b8575c862673d371a50d
Courtney Stow <courtney-stow@hotmail.co.uk>
2022-01-26 16:11:06 +0000

    Updated code/tests and added index.erb

--- app.rb
+++ app.rb
@@ -7,7 +7,7 @@ class Chitter < Sinatra::Base
   end
 
   get "/" do
-    "My first peep"
+    erb :index
   end
 
   run! if app_file == $0
--- spec/features/viewing_peeps_spec.rb
+++ spec/features/viewing_peeps_spec.rb
@@ -3,6 +3,6 @@ require "./app"
 feature "Viewing peeps" do
   scenario "Visiting the home page" do
     visit ("/")
-    expect(page).to have_content "My first peep"
+    expect(page).to have_content "Welcome to Chitter"
   end
 end
