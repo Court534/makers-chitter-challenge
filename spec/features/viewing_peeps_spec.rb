14329958e6d8fddcf208f98004bb85559e407fa7
Courtney Stow <courtney-stow@hotmail.co.uk>
2022-01-27 09:45:34 +0000

    Added posting a peep test and matching code. rspec passing 2/2

--- spec/features/viewing_peeps_spec.rb
+++ spec/features/viewing_peeps_spec.rb
@@ -1,8 +1,16 @@
 require "./app"
 
-feature "Viewing peeps" do
+feature "Viewing homepage" do
   scenario "Visiting the home page" do
     visit ("/")
     expect(page).to have_content "Welcome to Chitter"
+  end
+
+  feature "Posting a peep" do
+    scenario "A user is able to post a peep" do
+      visit ("/")
+      expect(page).to have_content "Cheep:"
+      click_button "Cheep!"
+    end
   end
 end
